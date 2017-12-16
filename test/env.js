b17742e4af133e60a699291bbe036f195dd1ad81
Yong Bakos <ybakos@humanoriented.com>
2017-12-16 10:11:48 -0800

    Remove test env test.

--- test/env.js
+++ test/env.js
@@ -1,7 +1,4 @@
 describe("Test environment variables", function() {
-  it("has NODE_ENV set to 'test'", function() {
-    expect(process.env.NODE_ENV).to.equal("test");
-  });
   it.skip("has the right database url", function() {
     expect(process.env.DATABASE_URL).to.equal("FAKE");
   });
