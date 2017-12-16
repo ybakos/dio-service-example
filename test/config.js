04efdf904967773515db5de4cae25b1dcad90e69
Yong Bakos <ybakos@humanoriented.com>
2017-12-16 08:12:55 -0800

    Configure mocha, set test env.

---
+++ test/config.js
@@ -0,0 +1,5 @@
+// Test suite configuration
+process.env.NODE_ENV = "test";
+
+let chai = require('chai');
+global.expect = chai.expect;
--- test/env.js
+++ test/env.js
@@ -1,7 +1,7 @@
-let chai = require('chai');
-let expect = chai.expect;
-
 describe("Test environment variables", function() {
+  it("has NODE_ENV set to 'test'", function() {
+    expect(process.env.NODE_ENV).to.equal("test");
+  });
   it("has the right database url", function() {
     expect(process.env.DATABASE_URL).to.equal("FAKE");
   });
