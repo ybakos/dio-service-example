c675881a7834cf63509adee1cdc02ff1bb408d4b
Yong Bakos <ybakos@humanoriented.com>
2017-12-16 10:09:57 -0800

    Simplify root response.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.status(200).send(''));
+app.get('/', (req, res) => res.status(200).send());
 
 app.get('/readings', (req, res) => {
   res.status(200).send();
