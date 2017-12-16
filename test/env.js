a7272874aebb420391bb7b701432fcd24eabf23b
Yong Bakos <ybakos@humanoriented.com>
2017-12-16 08:43:29 -0800

    Skip db url env test for now, we dont need it yet.

--- test/env.js
+++ test/env.js
@@ -2,7 +2,7 @@ describe("Test environment variables", function() {
   it("has NODE_ENV set to 'test'", function() {
     expect(process.env.NODE_ENV).to.equal("test");
   });
-  it("has the right database url", function() {
+  it.skip("has the right database url", function() {
     expect(process.env.DATABASE_URL).to.equal("FAKE");
   });
 });
