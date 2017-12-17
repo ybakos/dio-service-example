5008cce4086e412f976b6641bfc44f08c18f5e5c
Yong Bakos <ybakos@humanoriented.com>
2017-12-16 22:38:07 -0800

    Use dotenv-driven database config.

--- knexfile.js
+++ knexfile.js
@@ -1,39 +1,10 @@
-// Update with your config settings.
+if (process.env.NODE_ENV == 'development') {
+  require('dotenv').config({path: '.env'});
+} else if (process.env.NODE_ENV == 'test') {
+  require('dotenv').config({path: '.env.test'});
+}
 
 module.exports = {
-
-  development: {
-    client: 'mysql',
-    connection: process.env.DATABASE_URL,
-  },
-
-  test: {
-    client: 'mysql',
-    connection: process.env.DATABASE_URL,
-  },
-
-  staging: {
-    client: 'mysql',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'mysql',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+  client: 'mysql',
+  connection: process.env.DATABASE_URL,
 };
