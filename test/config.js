003f96bee7332a39b42897ca5ae694abf65ea111
Yong Bakos <ybakos@humanoriented.com>
2017-12-16 23:52:25 -0800

    Test config: Load knex and experiment with migrating and seeding test db.

--- test/config.js
+++ test/config.js
@@ -4,3 +4,7 @@ require('dotenv').config({path: '.env.test'});
 
 let chai = require('chai');
 global.expect = chai.expect;
+
+const knex = require('knex')(require('../knexfile'));
+knex.migrate.latest();
+knex.seed.run();
